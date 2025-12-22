421ad37ed133c75949e84b6ac0b81bc617c71f1c
Deal Bankers <dealbankers@gmail.com>
2025-12-22 12:29:36 -0600

    Add Firebase configuration file
    This file contains Firebase configuration settings, exposed on the window object for use in the main app script.

---
+++ config/firebase-config.js
@@ -0,0 +1,14 @@
+// config/firebase-config.js
+// This file is intentionally "no-imports" so it can be loaded by a plain <script src="..."></script>
+// It exposes your Firebase config on window for your main app script to read.
+
+window.__FIREBASE_CONFIG__ = {
+  apiKey: "AIzaSyD2pJU2kxrlmgEeBq3BhxHw0cMW8pxEXi8",
+  authDomain: "tire-rescue.firebaseapp.com",
+  databaseURL: "https://tire-rescue-default-rtdb.firebaseio.com",
+  projectId: "tire-rescue",
+  storageBucket: "tire-rescue.firebasestorage.app",
+  messagingSenderId: "914831772614",
+  appId: "1:914831772614:web:f341f6e35cc059647dc0c7",
+  measurementId: "G-K9B8V4500F"
+};
